6cf0e997857cee32b60ec40b0621326598cff3dd
Andrei Chayeuski <andreichayeuski@gmail.com>
2019-04-25 10:13:21 +0300

    fix config4

--- models/index.js
+++ models/index.js
@@ -9,6 +9,8 @@ module.exports = () => {
 
     if (process.env.NODE_ENV !== undefined)
     {
+        console.log('production');
+        console.log(config.db[process.env.NODE_ENV]);
         dbConfig = config.db[process.env.NODE_ENV];
     }
     console.log(dbConfig);
