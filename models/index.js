b0ce9c69e3fbffa35d860b427d5cf9126ff372ea
Andrei Chayeuski <andreichayeuski@gmail.com>
2019-04-25 10:22:21 +0300

    fix config finally

--- models/index.js
+++ models/index.js
@@ -5,14 +5,8 @@ const config = require('../config.json');
 
 module.exports = () => {
     console.log(process.env.NODE_ENV);
-    let dbConfig = config.db['development'];
+    let dbConfig = config.db[process.env.NODE_ENV || 'development'];
 
-    if (process.env.NODE_ENV !== undefined)
-    {
-        console.log('production');
-        console.log(config.db.production);
-        dbConfig = config.db.production;
-    }
     console.log(dbConfig);
     const options = {
         host: dbConfig.options.host,
